671ffac1024801dd00bdcffa39877a8d07b66c90
Jason Cavanaugh <jascav418@gmail.com>
2023-04-28 23:57:04 -0400

    Formatting changes

--- src/components/HabitDisplay.tsx
+++ src/components/HabitDisplay.tsx
@@ -19,6 +19,146 @@ import {
 } from "../utils/hooks/habitHooks";
 import { ColorOption, HabitWithDayDrops } from "../utils/types";
 
+interface IHabitDisplayProps {
+  habit: HabitWithDayDrops;
+  year: number;
+  color: ColorOption;
+}
+export const HabitDisplay = (props: IHabitDisplayProps) => {
+  const create_day_drop = use_create_day_drop();
+  const delete_day_drop = use_delete_day_drop();
+  console.log("HabitDisplay, habit_day_drops", props.habit.habit_day_drops);
+  const is_today_marked = determine_whether_today_is_marked(
+    props.habit.habit_day_drops
+  );
+  console.log("is_today_marked", is_today_marked);
+
+  return (
+    <li key={props.habit.id} className="rounded-lg border bg-white p-2 md:p-4">
+      <div className="flex justify-between">
+        <h1 className="flex justify-start text-xl font-semibold text-slate-700 md:text-2xl lg:text-3xl">
+          {props.habit.name}
+        </h1>
+        <div className="flex border border-red-500">
+          <div>
+          </div>
+          <button
+            className="rounded-full border bg-pink-500 px-4 text-sm font-semibold text-white hover:brightness-110 md:text-base"
+            onClick={(e) => {
+              e.preventDefault();
+              const payload = {
+                habit_id: props.habit.id,
+                year: new Date().getFullYear(),
+                month: new Date().getMonth() + 1,
+                day: new Date().getDate(),
+              };
+              is_today_marked
+                ? delete_day_drop.mutate(payload)
+                : create_day_drop.mutate(payload);
+            }}
+          >
+            {is_today_marked ? "Unmark today" : "Mark today"}
+          </button>
+        </div>
+      </div>
+      <div className="h-2 md:h-4" />
+      <div className="flex gap-0">
+        <div className="mt-[-0.25rem] mr-2 mb-4 flex flex-col justify-around text-xs lg:mr-4 lg:text-base">
+          <p>Sun</p>
+          <p>Mon</p>
+          <p>Tue</p>
+          <p>Wed</p>
+          <p>Thu</p>
+          <p>Fri</p>
+          <p>Sat</p>
+        </div>
+        <div className="overflow-x-auto">
+          <div className="jason mb-4 gap-[0.15rem] md:gap-[0.2rem] lg:gap-[0.3rem]">
+            <HabitSquaresDisplay
+              color={props.color}
+              habit={props.habit}
+              year={props.year}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="h-2 md:h-4" />
+      <DeleteHabit id={props.habit.id} />
+    </li>
+  );
+};
+
+export default HabitDisplay;
+
+interface IHabitSquaresDisplay {
+  habit: HabitWithDayDrops;
+  year: number;
+  color: ColorOption;
+}
+const HabitSquaresDisplay = ({
+  habit,
+  year,
+  color
+}: IHabitSquaresDisplay) => {
+  const create_day_drop = use_create_day_drop();
+  const delete_day_drop = use_delete_day_drop();
+  const [number_of_days_in_year, first_day_of_year] = useMemo(
+    () => [
+      get_number_of_days_in_year(year),
+      get_first_day_of_year(year),
+    ],
+    []
+  );
+
+  //UI
+  let output = [];
+  for (let i = 1; i < first_day_of_year; i++) {
+    output.push(
+      <div
+        key={i - first_day_of_year}
+        className="h-[20px] w-[20px] opacity-0"
+      />
+    );
+  }
+  const day_out_of_year_for_today = get_day_out_of_year(new Date());
+  let i = 1;
+  for (; i <= day_out_of_year_for_today; i++) {
+    const is_checked = check_if_marked(i, habit.habit_day_drops, year);
+    const [month, day] = get_day_and_month(i, year);
+    if (!month || !day) {
+      throw new Error("!month || !day");
+    }
+    const day_name = get_day_name(year, month, day);
+    output.push(
+      <HabitDayDropTooltip
+        key={i}
+        color={color}
+        is_checked={is_checked}
+        on_click={() => {
+          const payload = {
+            habit_id: habit.id,
+            year: year,
+            month: month,
+            day: day,
+          };
+          is_checked
+            ? delete_day_drop.mutate(payload)
+            : create_day_drop.mutate(payload);
+        }}
+        content={`${day_name} ${month}-${day}`}
+      />
+    );
+  }
+  for (; i <= number_of_days_in_year; i++) {
+    output.push(
+      <div
+        key={i}
+        className={`h-[20px] w-[20px] rounded-sm border border-${color} opacity-30 md:rounded md:border lg:h-[30px] lg:w-[30px]`}
+      ></div>
+    );
+  }
+  return <>{output}</>;
+};
 interface IHabitDayDropTooltipProps {
   is_checked: boolean;
   on_click: () => void;
@@ -36,9 +176,8 @@ function HabitDayDropTooltip({
       <Tooltip.Root>
         <Tooltip.Trigger asChild>
           <div
-            className={`h-[20px] w-[20px] rounded-sm border border-${color} hover:cursor-pointer hover:brightness-110 md:rounded md:border lg:h-[30px] lg:w-[30px] ${
-              is_checked ? "bg-" + color : ""
-            }`}
+            className={`h-[20px] w-[20px] rounded-sm border border-${color} hover:cursor-pointer hover:brightness-110 md:rounded md:border lg:h-[30px] lg:w-[30px] ${is_checked ? "bg-" + color : ""
+              }`}
             onClick={on_click}
           />
         </Tooltip.Trigger>
@@ -60,7 +199,6 @@ interface IDeleteHabitProps {
   id: string;
 }
 const DeleteHabit = ({ id }: IDeleteHabitProps) => {
-  const [loading, set_loading] = useState(false);
   const [is_modal_open, set_is_modal_open] = useState(false);
 
   const api_utils = api.useContext();
@@ -70,7 +208,7 @@ const DeleteHabit = ({ id }: IDeleteHabitProps) => {
       // set_loading(false);
       set_is_modal_open(false); //TODO: Have to figure out how to close the modal once the new data comes in
     },
-    onError: (err, data, ctx) => {
+    onError: () => {
       alert("error");
     },
   });
@@ -128,145 +266,4 @@ const DeleteHabit = ({ id }: IDeleteHabitProps) => {
   );
 };
 
-interface IHabitSquaresDisplay {
-  number_of_days_in_year: number;
-  first_day_of_year: number;
-  habit: HabitWithDayDrops;
-  year: number;
-  color: ColorOption;
-}
-const HabitSquaresDisplay = ({
-  number_of_days_in_year,
-  first_day_of_year,
-  habit,
-  year,
-  color
-}: IHabitSquaresDisplay) => {
-  const create_day_drop = use_create_day_drop();
-  const delete_day_drop = use_delete_day_drop();
 
-  //UI
-  let output = [];
-  for (let i = 1; i < first_day_of_year; i++) {
-    output.push(
-      <div
-        key={i - first_day_of_year}
-        className="h-[20px] w-[20px] opacity-0"
-      ></div>
-    );
-  }
-  const day_out_of_year_for_today = get_day_out_of_year(new Date());
-  let i = 1;
-  for (; i <= day_out_of_year_for_today; i++) {
-    const is_checked = check_if_marked(i, habit.habit_day_drops, year);
-    const [month, day] = get_day_and_month(i, year);
-    if (!month || !day) {
-      throw new Error("!month || !day");
-    }
-    const day_name = get_day_name(year, month, day);
-    output.push(
-      <HabitDayDropTooltip
-        key={i}
-        color={color}
-        is_checked={is_checked}
-        on_click={() => {
-          const payload = {
-            habit_id: habit.id,
-            year: year,
-            month: month,
-            day: day,
-          };
-          is_checked
-            ? delete_day_drop.mutate(payload)
-            : create_day_drop.mutate(payload);
-        }}
-        content={`${day_name} ${month}-${day}`}
-      />
-    );
-  }
-  for (; i <= number_of_days_in_year; i++) {
-    output.push(
-      <div
-        key={i}
-        className={`h-[20px] w-[20px] rounded-sm border border-${color} opacity-30 md:rounded md:border lg:h-[30px] lg:w-[30px]`}
-      ></div>
-    );
-  }
-  return <>{output}</>;
-};
-
-interface IHabitDisplayProps {
-  habit: HabitWithDayDrops;
-  year: number;
-  color: ColorOption;
-}
-export const HabitDisplay = (props: IHabitDisplayProps) => {
-  const create_day_drop = use_create_day_drop();
-  const delete_day_drop = use_delete_day_drop();
-  const [number_of_days_in_year, first_day_of_year] = useMemo(
-    () => [
-      get_number_of_days_in_year(props.year),
-      get_first_day_of_year(props.year),
-    ],
-    []
-  );
-  console.log("HabitDisplay, habit_day_drops", props.habit.habit_day_drops);
-  const is_today_marked = determine_whether_today_is_marked(
-    props.habit.habit_day_drops
-  );
-  console.log("is_today_marked", is_today_marked);
-
-  return (
-    <li key={props.habit.id} className="rounded-lg border bg-white p-2 md:p-4">
-      <div className="flex justify-between">
-        <h1 className="flex justify-start text-xl font-semibold text-slate-700 md:text-2xl lg:text-3xl">
-          {props.habit.name}
-        </h1>
-        <button
-          className="rounded-full border bg-pink-500 px-4 text-sm font-semibold text-white hover:brightness-110 md:text-base"
-          onClick={(e) => {
-            e.preventDefault();
-            const payload = {
-              habit_id: props.habit.id,
-              year: new Date().getFullYear(),
-              month: new Date().getMonth() + 1,
-              day: new Date().getDate(),
-            };
-            is_today_marked
-              ? delete_day_drop.mutate(payload)
-              : create_day_drop.mutate(payload);
-          }}
-        >
-          {is_today_marked ? "Unmark today" : "Mark today"}
-        </button>
-      </div>
-      <div className="h-2 md:h-4" />
-      <div className="flex gap-0">
-        <div className="mt-[-0.25rem] mr-2 mb-4 flex flex-col justify-around text-xs lg:mr-4 lg:text-base">
-          <p>Sun</p>
-          <p>Mon</p>
-          <p>Tue</p>
-          <p>Wed</p>
-          <p>Thu</p>
-          <p>Fri</p>
-          <p>Sat</p>
-        </div>
-        <div className="overflow-x-auto">
-          <div className="jason mb-4 gap-[0.15rem] md:gap-[0.2rem] lg:gap-[0.3rem]">
-            <HabitSquaresDisplay
-              color={props.color}
-              number_of_days_in_year={number_of_days_in_year}
-              first_day_of_year={first_day_of_year}
-              habit={props.habit}
-              year={props.year}
-            />
-          </div>
-        </div>
-      </div>
-      <div className="h-2 md:h-4" />
-      <DeleteHabit id={props.habit.id} />
-    </li>
-  );
-};
-
-export default HabitDisplay;
